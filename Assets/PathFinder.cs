d8432691317b623cff21095fad0fdbb7f0ae9fc0
MathieuDijkstra <palcopy@hotmail.com>
2019-12-15 19:55:36 +0100

    start path finder script (dictinory)

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    Dictionary<Vector3, Waypoint> grid = new Dictionary<Vector3, Waypoint>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
